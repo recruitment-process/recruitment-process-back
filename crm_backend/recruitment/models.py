5f4e6b6a3bbf60e9f6e13a0392002ab126d5cf2c
Mikhail <mishatar2004@gmail.com>
2023-07-25 14:44:27 +0300

    bug fix: fix model

--- crm_backend/recruitment/models.py
+++ crm_backend/recruitment/models.py
@@ -1,106 +1,128 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import RegexValidator
 from django.db import models
 from multiselectfield import MultiSelectField
-from django.core.validators import RegexValidator
-from .utils import (EXPERIENCE, EMPLOYMENT_TYPE, SCHEDULE_WORK)
+
+from .utils import EMPLOYMENT_TYPE, EXPERIENCE, SCHEDULE_WORK
 
 User = get_user_model()
 
 
 class Company(models.Model):
-    ''' Информация о компании '''
+    """Информация о компании."""
+
     company_title = models.CharField(
         max_length=100,
-        verbose_name='Название компании',
+        verbose_name="Название компании",
     )
     about_company = models.TextField(
-        verbose_name = 'О компании',
-        help_text='Введите информацию о компани'
+        verbose_name="О компании",
+        help_text="Введите информацию о компани",
     )
 
     company_address = models.CharField(
         max_length=100,
-        verbose_name='Адрес компании',
+        verbose_name="Адрес компании",
     )
 
-    email = models.EmailField(verbose_name='Эл.почта',)
+    email = models.EmailField(
+        verbose_name="Эл.почта",
+    )
 
     phoneNumberRegex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
 
     phoneNumber = models.CharField(
         validators=[phoneNumberRegex],
         max_length=16,
+        null=True,
     )
 
-    link_hr = models.URLField(max_length=100, verbose_name = 'Ссылка на HR',)
-
+    link_hr = models.URLField(
+        max_length=100,
+        verbose_name="Ссылка на HR",
+        null=True,
+    )
 
     class Meta:
-        ordering = ['company_title']
-        verbose_name = 'Основная информация о компании'
+        ordering = ["company_title"]
+        verbose_name = "Основная информация о компании"
 
     def __str__(self):
         return self.company_title
 
 
 class Vacancy(models.Model):
-    ''' Информация о вакансии '''
-
-    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, verbose_name='Компания', related_name='vacancys')
-    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, verbose_name='Автор', related_name='vacancys')
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Компания",
+        related_name="vacancys",
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Автор",
+        related_name="vacancys",
+    )
 
     vacancy_title = models.CharField(
         max_length=100,
-        verbose_name='Название вакансии',
+        verbose_name="Название вакансии",
     )
 
     required_experience = models.CharField(
         max_length=1,
         choices=EXPERIENCE,
-        verbose_name='Требуемый опыт работы',
+        verbose_name="Требуемый опыт работы",
         blank=False,
     )
-    
+
     employment_type = MultiSelectField(
         max_length=3,
         choices=EMPLOYMENT_TYPE,
-        verbose_name='Тип занятости',
+        verbose_name="Тип занятости",
         blank=False,
     )
 
     schedule_work = MultiSelectField(
         max_length=3,
         choices=SCHEDULE_WORK,
-        verbose_name='Расписание работы',
+        verbose_name="Расписание работы",
         blank=False,
     )
 
     about_company = models.TextField(
-        verbose_name = 'О компании',
-        help_text='Введите информацию о компани'
+        verbose_name="О компании",
+        help_text="Введите информацию о компани",
     )
 
     pub_date = models.DateTimeField(
-        'Дата публикации вакансии',
-        auto_now_add=True)
-    
+        "Дата публикации вакансии",
+        auto_now_add=True,
+    )
+
     job_conditions = models.TextField(
-        verbose_name = 'Условия работы',
-        help_text='Введите условия работы'
+        verbose_name="Условия работы",
+        help_text="Введите условия работы",
     )
 
     job_responsibilities = models.TextField(
-        verbose_name = 'Обязанности кондидата',
-        help_text='Введите обязанности кондидата'
+        verbose_name="Обязанности кондидата",
+        help_text="Введите обязанности кондидата",
     )
 
-    technology_stack = models.TextField( # позже можно будет сделать бд со всеми навыками 
-        verbose_name = 'Ключевые навыки',
+    technology_stack = (
+        # позже можно будет сделать бд со всеми навыками
+        models.TextField(
+            verbose_name="Ключевые навыки",
+        )
     )
 
     class Meta:
-        ordering = ['pub_date']
-        verbose_name = 'Основная информация о вакансии'
+        ordering = ["pub_date"]
+        verbose_name = "Основная информация о вакансии"
 
     def __str__(self):
         return self.vacancy_title
--- crm_backend/users/admin.py
+++ crm_backend/users/admin.py
@@ -6,38 +6,68 @@ from .models import Category, User
 
 @admin.register(User)
 class CustomUserAdmin(UserAdmin):
-    ''' Переопределены поля формы для исключения поля username. '''
+    """Переопределены поля формы для исключения поля username."""
+
     model = User
 
     fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        ('Personal info', {
-            'fields': ('first_name', 'last_name', 'patronymic',
-                       'role', 'category', 'telegram', 'photo')
-        }),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
-                                    'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        (None, {"fields": ("email", "password")}),
+        (
+            "Personal info",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "patronymic",
+                    "role",
+                    "category",
+                    "telegram",
+                    "photo",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
     )
     add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
     )
-    readonly_fields = ('last_login', 'date_joined',)
-    list_display = ('email', 'first_name', 'last_name',
-                    'role', 'category', 'is_staff')
-    list_filter = ('email', 'first_name', 'last_name', 'role', 'category')
-    search_fields = ('email', 'first_name', 'last_name', )
-    empty_value_display = '-пусто-'
-    ordering = ('-id',)
+    readonly_fields = (
+        "last_login",
+        "date_joined",
+    )
+    list_display = ("email", "first_name", "last_name", "role", "category", "is_staff")
+    list_filter = ("email", "first_name", "last_name", "role", "category")
+    search_fields = (
+        "email",
+        "first_name",
+        "last_name",
+    )
+    empty_value_display = "-пусто-"
+    ordering = ("-id",)
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', )
-    list_filter = ('name', )
-    search_fields = ('name',)
-    empty_value_display = '-пусто-'
-    ordering = ('name',)
+    list_display = ("name",)
+    list_filter = ("name",)
+    search_fields = ("name",)
+    empty_value_display = "-пусто-"
+    ordering = ("name",)
